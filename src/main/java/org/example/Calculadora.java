b7422067f9fc3abaadff193a36c1d3545e63a6a3
Javier <javi.ssweb@gmail.com>
2023-01-22 19:50:26 +0100

    Calculadora de IMC

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,2 +1,61 @@
-package org.example;public class Calculadora {
+package org.example;
+
+public class Calculadora {
+
+    private String nombre;
+    private Integer edad;
+    private Double altura;
+    private Double peso;
+
+    public Calculadora() {
+    }
+
+    public Calculadora(String nombre, Integer edad, Double altura, Double peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculadora{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculadora imc = new Calculadora();
+        Scanner entrada = new Scanner(System.in);
+        /*System.out.println("Indica cual es tu genero : /n1 Hombre (H) /n2 Mujer (M) ");
+        String genero = entrada.nextLine();*/
+        System.out.println("Hola, introduce tu nombre ");
+        String nombre = entrada.nextLine();
+        System.out.println("Hola " + nombre + " " + "Introduce tu altura ");
+        int altura = entrada.nextInt();
+        if (altura < 0 || altura >= 3) {
+            System.out.println("Introduce una altura correcta ");
+        }
     }
 }
